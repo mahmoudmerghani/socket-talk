bdf2f20edc619dee8446e15f2f62fe150d16bf42
Mahmoud Mirghani <mahmoudtawfiq777@gmail.com>
2026-07-10 23:24:28 +0300

    Change api contract

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -2,24 +2,63 @@ import type { Endpoints, Bodies } from "@socket-talk/shared/endpoints.js";
 
 const api_url = import.meta.env.VITE_API_URL;
 
-export async function api<
+type ResponseHandler = (res: Response) => void;
+
+let responseHandlers: ResponseHandler[] = [];
+
+export function addResponseHandler(handler: ResponseHandler) {
+    responseHandlers.push(handler);
+}
+
+export function removeResponseHandler(handler: ResponseHandler) {
+    responseHandlers = responseHandlers.filter((h) => h !== handler);
+}
+
+type Api = <
     Path extends keyof Endpoints,
     Method extends keyof Endpoints[Path] & string,
     Body extends Endpoints[Path][Method] & Bodies<unknown, unknown>,
 >(
     path: Path,
-    method: Method,
-    ...body: Body["requestBody"] extends never ? [] : [Body["requestBody"]]
-): Promise<Body["responseBody"]> {
-    try {
-        const res = await fetch(`${api_url}${path}`, {
-            body: JSON.stringify(body[0]),
-            method,
-            headers: {
+    init: Body["requestBody"] extends never
+        ? {
+              method: Method;
+              headers?: Record<string, string>;
+          }
+        : {
+              method: Method;
+              body: Body["requestBody"];
+              headers?: Record<string, string>;
+          },
+) => Promise<Body["responseBody"]>;
+
+export const api: Api = async (path, init) => {
+    const requestInit: RequestInit = {
+        method: init.method,
+        credentials: "include",
+        headers: {
+            ...init.headers,
+        },
+    };
+
+    if ("body" in init) {
+        if (init.body instanceof FormData) {
+            requestInit.body = init.body;
+        } else {
+            requestInit.body = JSON.stringify(init.body);
+            requestInit.headers = {
                 "Content-Type": "application/json",
-            },
-            credentials: "include",
-        });
+                ...init.headers,
+            };
+        }
+    }
+
+    try {
+        const res = await fetch(`${api_url}${path}`, requestInit);
+
+        for (const handler of responseHandlers) {
+            handler(res);
+        }
 
         if (!res.ok) {
             return {
@@ -37,4 +76,4 @@ export async function api<
             status: -1,
         };
     }
-}
+};
