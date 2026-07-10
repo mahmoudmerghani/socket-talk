c6fe461b27a4f5965ca49a0a7bfb8654ae4580bb
Mahmoud Mirghani <mahmoudtawfiq777@gmail.com>
2026-07-10 17:17:37 +0300

    Add cors and global error handler

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,12 +2,38 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 import authRouter from "./routes/authRouter.js";
+import cors from "cors";
+import { HttpError } from "./utils/HttpError.js";
+import type { ErrorRequestHandler } from "express";
 
 const app = express();
 
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    }),
+);
+
+app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use("/auth", authRouter);
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    if (err instanceof HttpError) {
+        return res.status(err.status).json({
+            error: err.message,
+        });
+    }
+
+    console.error(err);
+    return res.status(500).json({
+        error: "Server error",
+    });
+};
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, (err) => {
     if (err) throw err;
