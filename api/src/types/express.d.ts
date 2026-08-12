9e14a000db1b7d09882686b1c438c136b9acf13c
Mahmoud Mirghani <mahmoudtawfiq777@gmail.com>
2026-08-12 22:05:24 +0300

    Change the type of validatedData

--- api/src/types/express.d.ts
+++ api/src/types/express.d.ts
@@ -3,6 +3,10 @@ import type { User } from "../../generated/prisma/client.js";
 declare module "express-serve-static-core" {
     interface Request {
         user: Omit<User, "password">;
-        validatedData: Record<string, unknown>;
+        validatedData: {
+            body?: unknown;
+            query?: unknown;
+            params?: unknown;
+        };
     }
 }
